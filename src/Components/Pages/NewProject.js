9bd190f1e5bcdb5ba1f63393b89fb6770b60e7ca
otavio <barbosaotavio293@gmail.com>
2024-03-27 19:07:56 -0300

    aula 22

--- src/Components/Pages/NewProject.js
+++ src/Components/Pages/NewProject.js
@@ -7,7 +7,7 @@ function NewProject() {
        <div className={styles.newproject_container}>
         <h1>Crie seu projeto</h1>
         <p>Crie seu projeto para depois adiconar os serviços</p>
-        <ProjectForm/>
+        <ProjectForm btnText='Criar projeto'/>
        </div>
     )
 }
--- src/Components/Project/ProjectForm.js
+++ src/Components/Project/ProjectForm.js
@@ -1,20 +1,23 @@
-function ProjectForm () {
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+import sytles from './ProjectForm.module.css'
+
+function ProjectForm ({btnText}) {
     return (
-        <form>
-            <div>
-                 <input type='text' placeholder='insira o nome do projeto' />
-            </div>
-            <div>
-                <input type='number' placeholder='insira o orçamento total'/>
-            </div>
-            <div>
-            <select name='category_id'>
-             <option disabled>selecione a categoria</option>   
-            </select>
-            </div>
-            <div>
-                <input type='submit' value='criar projeto'/>
-            </div>
+        <form className={sytles.form}>
+           <Input type='text' 
+           text='Nome do projeto'
+            name='name' 
+            placeholder='Insira o nome do projeto'/>
+            
+            <Input type='number' 
+           text='Orçamento do projeto'
+            name='budget' 
+            placeholder='Insira o orçamento total'/>
+
+            <Select name='category_id' text='Selecione a categoria' />
+            <SubmitButton text={btnText} />
             
         </form>
     )
