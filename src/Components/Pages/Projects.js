2cfbff60f69dfff4a55429aa12487fcdb7430ae4
otavio <barbosaotavio293@gmail.com>
2024-11-23 20:18:32 -0300

    Mensagens do Sistema

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,10 +1,20 @@
+import { useLocation } from 'react-router-dom'
+
 import Message from '../layout/Message'
 
 function Project () {
+
+    const location = useLocation()
+    let message =''
+
+    if(location.state) {
+        message = location.state.message
+    }
+
     return (
         <div>
         <h1>Meus projetos</h1>
-        <Message msg={'Alguma mensagem'}/>
+        {message && <Message type='success' msg={message}/>}
         </div>
     )
 }
--- src/Components/layout/Message.js
+++ src/Components/layout/Message.js
@@ -1,33 +1,33 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
+import styles from './Message.module.css';
 
-import styles from './Message.module.css'
-
-function Message({type,msg}) {
-    const [visible, setVisible] = useState(false)
+function Message({ type, msg }) {
+    const [visible, setVisible] = useState(false);
 
     useEffect(() => {
-        
-        if(!msg) {
+        if (!msg) {
             setVisible(false)
             return
         }
 
-        setVisible(true)
+        setVisible(true); 
 
         const timer = setTimeout(() => {
-            setVisible(false)
-        },3000)
+            setVisible(false);
+        }, 3000);
 
+       
         return () => clearTimeout(timer)
- 
-    },[msg])
+        },[msg])
+
+    
+    if (!visible) return null
 
     return (
-        <>
-        <div className={`${styles.message} ${styles[type]}`}>{msg}</div>
-        
-        </>
+        <div className={`${styles.message} ${styles[type]}`}>
+            {msg}
+        </div>
     )
 }
 
-export default Message
+export default Message;
