38ec865d75e8486a226b92828aedeadf91c3613b
otavio <barbosaotavio293@gmail.com>
2024-12-15 00:50:09 -0300

    Criando componente de loader

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 
 import Message from '../layout/Message'
 import Container from '../layout/Container'
+import Loading from '../layout/Loading'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../Project/ProjectCard'
 
@@ -10,6 +11,7 @@ import styles from './Projects.module.css'
 
 function Project () {
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message =''
@@ -19,17 +21,21 @@ function Project () {
     }
 
     useEffect( () => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'content-type': 'application/json',
-            },
-        }).then(resp => resp.json())
-          .then(data => {
-            console.log(data)
-            setProjects(data)
-          })
-          .catch((err) => console.log(err))
+      setTimeout (
+        () => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json',
+                },
+            }).then(resp => resp.json())
+              .then(data => {
+                console.log(data)
+                setProjects(data)
+                setRemoveLoading(true)
+              })
+              .catch((err) => console.log(err))
+        }, 300)
     },[])
 
     return (
@@ -48,6 +54,10 @@ function Project () {
             category={project.category.name}
             key={project.id}
             />))}
+            {!removeLoading && <Loading/>}
+            {removeLoading && projects.length === 0 && (
+                <p>Não há projetos cadastrados!</p>
+            )}
         </Container>
         </div>
     )
